ad18c9eec1ecc8312a4857b88ac37eb45ed74b3f
LeonarEQP <leonar.estupinanquinonez@plexus.es>
2024-06-24 13:36:58 +0200

    build(styles) Instalado Angular Material y PrimeFlex

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { LayoutPageComponent } from './auth/pages/layout-page/layout-page.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 @NgModule({
   declarations: [AppComponent, LayoutPageComponent],
   imports: [BrowserModule, AppRoutingModule, SharedModule],
-  providers: [],
+  providers: [provideAnimationsAsync()],
   bootstrap: [AppComponent],
 })
 export class AppModule {}

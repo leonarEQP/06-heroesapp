d902952b4a51975c1509f516e88c9a7a5b138a20
LeonarEQP <leonar.estupinanquinonez@plexus.es>
2024-06-25 09:14:48 +0200

    feat(assets): descarga de imágenes para HeroImagePipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, SharedModule],
+  imports: [BrowserModule, AppRoutingModule, SharedModule, HttpClientModule],
   providers: [provideAnimationsAsync()],
   bootstrap: [AppComponent],
 })
--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -5,17 +5,19 @@ import { HeroesRoutingModule } from './heroes-routing.module';
 import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 import { MaterialModule } from '../material/material.module';
 import { HeroPageComponent } from './pages/hero-page/hero-page.component';
-import { ListPageComponent } from './pages/list-page/list-page.component';
 import { NewPageComponent } from './pages/new-page/new-page.component';
 import { SearchPageComponent } from './pages/search-page/search-page.component';
+import { ListPageComponent } from './pages/list-page/list-page.component';
+import { CardComponent } from './components/card/card.component';
 
 @NgModule({
   declarations: [
     LayoutPageComponent,
-    HeroPageComponent,
     ListPageComponent,
+    HeroPageComponent,
     NewPageComponent,
     SearchPageComponent,
+    CardComponent,
   ],
   imports: [CommonModule, HeroesRoutingModule, MaterialModule],
 })
